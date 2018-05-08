aee3fcefcd2ab76c325d84035f9920b5c407f4c2
ghovat <georg@sattler.io>
2018-05-08 12:39:23 +0200

    update response object and logging

--- src/routes.php
+++ src/routes.php
@@ -13,7 +13,7 @@ $app->get('/ping', function (Request $request, Response $response, array $args) 
 
 // Test Authentication
 $app->post('/validate_credentials', function (Request $request, Response $response) {
-    $tid = \Ramsey\Uuid\Uuid::uuid4();
+    $tid =  \Ramsey\Uuid\Uuid::uuid4()->toString();
     $this->logger->info("got new request to validate credentials with ID: " . $tid);
 
     $data = array(
@@ -21,7 +21,6 @@ $app->post('/validate_credentials', function (Request $request, Response $respon
         "message" => "",
         "request_id" => $tid
     );
-    $newResponse = $response->withHeader('Content-type', 'application/json');
 
     $cType = $request->getContentType();
     if ($cType !== "application/json") {
@@ -29,8 +28,7 @@ $app->post('/validate_credentials', function (Request $request, Response $respon
         $this->logger->info($tid . ": invalid content type abort transaction");
         $data["message"] = "invalid content type";
 
-        $newResponse->withStatus(401);
-        $newResponse->write(json_encode($data));
+        return $response->withJson($data, 400);
     }
 
     try {
@@ -42,10 +40,12 @@ $app->post('/validate_credentials', function (Request $request, Response $respon
             $this->logger->info($tid . ": missing api key in post data");
             $data["message"] = "please submit a api key";
 
-            $newResponse->withStatus(401);
-            $newResponse->write(json_encode($data));
-            return $newResponse;
+            return $response->withJson($data, 400);
         }
+
+        $this->logger->info("-----------------");
+        $this->logger->info($api_key);
+        $this->logger->info("-----------------");
 
 
         \Stripe\Stripe::setApiKey($api_key);
@@ -55,63 +55,47 @@ $app->post('/validate_credentials', function (Request $request, Response $respon
 
         \Stripe\Stripe::setApiKey(null);
 
-        $newResponse->withStatus(200);
         $data["status"] = "OK";
         $data["message"] = "api key is valid";
         $data["api_feedback"] = $strip_data;
 
-        $newResponse->write(json_encode($data));
-        return $newResponse;
+        return $response->withJson($data, 200);
 
     } catch (\Stripe\Error\RateLimit $e) {
         $this->logger->info($tid . ": Rate Limit exceeded not possible to authenticate");
-        $newResponse->withStatus(500);
+        $response->withStatus(500);
         $data["message"] = "Rate Limit exceeded not possible to validate credentials, try again later";
-        $newResponse->write(json_encode($data));
-        return $newResponse;
-
+        return $response->withJson($data, 500);
     } catch (\Stripe\Error\InvalidRequest $e) {
         $this->logger->info($tid . ": invalid request");
         $this->logger->info($e);
-        $newResponse->withStatus(500);;
         $data["message"] = "Unkown error..";
-        $newResponse->write(json_encode($data));
-        return $newResponse;
+        return $response->withJson($data, 500);
 
     } catch (\Stripe\Error\Authentication $e) {
         $this->logger->info($tid . ": invalid credentials...");
         $this->logger->info($e);
-        $newResponse->withStatus(400);;
         $data["message"] = "invalid credentials";
-        $newResponse->write(json_encode($data));
-        return $newResponse;
+        return $response->withJson($data, 400);
     } catch (\Stripe\Error\Permission $e) {
         $this->logger->info($tid . ": invalid credentials");
         $this->logger->info($e);
-        $newResponse->withStatus(400);;
         $data["message"] = "Please ise a secret API ke";
-        $newResponse->write(json_encode($data));
-        return $newResponse;
+        return $response->withJson($data, 400);
     } catch (\Stripe\Error\ApiConnection $e) {
         $this->logger->info($tid . ": communication error with stripe api");
         $this->logger->info($e);
-        $newResponse->withStatus(500);;
         $data["message"] = "unkown error";
-        $newResponse->write(json_encode($data));
-        return $newResponse;
+        return $response->withJson($data, 500);
     } catch (\Stripe\Error\Base $e) {
         $this->logger->info($tid . ": stripe base error");
         $this->logger->info($e);
-        $newResponse->withStatus(500);;
         $data["message"] = "unkown error";
-        $newResponse->write(json_encode($data));
-        return $newResponse;
+        return $response->withJson($data, 500);
     } catch (Exception $e) {
         $this->logger->info($tid . ": communication with stripe broken because unkown error");
         $this->logger->info($e);
-        $newResponse->withStatus(500);;
         $data["message"] = "unkown error";
-        $newResponse->write(json_encode($data));
-        return $newResponse;
+        return $response->withJson($data, 500);
     }
 });
